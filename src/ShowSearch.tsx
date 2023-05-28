b0368e1335debca49e2b2ad4ab25abd8a7cf9017
Joe Smith <joseffsmith@icloud.com>
2023-05-28 23:01:39 +0100

    more styling tweaks

--- src/ShowSearch.tsx
+++ src/ShowSearch.tsx
@@ -20,10 +20,9 @@ export const ShowSearch = () => {
       <Box
         sx={{
           display: "flex",
-          alignItems: "baseline",
           maxWidth: 600,
           flexGrow: 1,
-          columnGap: { xs: 0, md: 1 },
+          columnGap: 1,
         }}
       >
         <Input
@@ -32,11 +31,14 @@ export const ShowSearch = () => {
           onChange={(e) => setBrandId(e.target.value)}
           placeholder="e.g. b01dmw9x of https://www.bbc.co.uk/sounds/brand/b01dmw9x"
           endDecorator={
-            <Link to={"/" + brandId}>
-              <Button variant={"soft"} disabled={!brandId}>
+            <Button variant={"soft"} disabled={!brandId}>
+              <Link
+                style={{ textDecoration: "none", color: "inherit" }}
+                to={"/" + brandId}
+              >
                 Load
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           }
         />
 
